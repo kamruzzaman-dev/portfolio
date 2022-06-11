ca6ae85c33b5cad7f22626b7754bd42d4fc0ffb3
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-06-11 18:14:18 +0600

    bannar page contact me button dynamic link added

--- src/Components/Bannar/Bannar.js
+++ src/Components/Bannar/Bannar.js
@@ -5,11 +5,13 @@ import jsImage from '../../assets/JS_image.png';
 import cssImage from '../../assets/css_image.png';
 import htmlImage from '../../assets/html _image.png';
 import tailwindImage from '../../assets/tailwind_image.png';
+import { Link } from 'react-router-dom';
 
 const Bannar = () => {
+
     return (
         <section className="">
-            <div className="hero min-h-screen bg-cover bg-no-repeat bg-center border-2 border-red-500" style={{}}>
+            <div className="hero min-h-screen bg-cover bg-no-repeat bg-center" style={{}}>
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className='w-full lg:w-1/2'>
                         <div className='flex items-center p-10 border-2 bg-red-200 border-sky-300'>
@@ -24,7 +26,7 @@ const Bannar = () => {
                             <h1 className='text-xl'>LOOKING FOR JOBS</h1>
                         </div>
                         <div className='mt-5'>
-                            <button className="btn btn-primary uppercase text-white font-bold">contact me</button>
+                            <Link to='/contact' className="btn btn-primary uppercase text-white font-bold">contact me</Link>
                             <button onClick={()=>{window.open("https://drive.google.com/file/d/1yhrsJpWfAiYCfTUExa0PBsTWI1kVD6QN/view?usp=sharing", "_blank")}} className="btn btn-error ml-4 uppercase text-white font-bold">resume</button>
                         </div>
                     </div>
