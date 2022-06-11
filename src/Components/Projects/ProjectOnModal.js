b58c65d349fb1e0d12ce0fa3f871ab3d697a4344
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-06-11 13:18:54 +0600

    project on module buttom probelm 50% sloved

--- src/Components/Projects/ProjectOnModal.js
+++ src/Components/Projects/ProjectOnModal.js
@@ -1,31 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './projectOnModal.css';
+import profilePhoto from '../../assets/profile_photo.png';
 
 const ProjectOnModal = ({ showigDetailsProject, setShowigDetailsProject }) => {
     // console.log(showigDetailsProject);
-   const {id, name, image1,about} = showigDetailsProject;
+    const { id, name, image1, image2, image3, image4, about } = showigDetailsProject;
     return (
         <>
             <input type="checkbox" id="ModalDeatilsShowingTigger" class="modal-toggle" />
-            <div class="modal modal-bottom sm:modal-middle">
-                <div class="modal-area relative hero bg-slate-200" >
-                    <div class="hero-content flex-col lg:flex-row-reverse justify-center items-center">
-                        <div class="text-center lg:text-left">
-                            <h1 class="text-3xl font-bold">{name}</h1>
-                            <p class="py-1">About:  {about}</p>
-                            <p class="py-6">ID: {id}</p>
-
-
+            <div class="modal modal-bottom  sm:modal-middle">
+                <div class="modal-area relative about-container border-2 border-red-400 card hero bg-slate-200" >
+                    {/* modal content */}
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className='card-header card-image '>
+                            <img width={400} src={image1} className="" alt='zaman profile pic' />
                         </div>
-                        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 modal-card-area">
-                            <div class="card w-96 bg-base-100 shadow-xl image-full">
-                                <figure><img src={image1} alt={name} /></figure>
+                        <div className='w-full lg:w-1/2'>
+                            <h1 class="text-3xl font-bold capitalize">{name}!</h1>
+                            <p class="pt-4 text-md font-bold text-red-400">{about}</p>
+                            <h1 class="text-3xl font-bold">Personal Informations</h1>
+                            <div className='p-4 flex flex-col md:flex-row justify-between w-full'>
+                                <div className=''>
+                                    <p>First Name: md</p>
+                                    <p>First Name: kamruzzaman</p>
+                                    <p>address: Brahamanbaria, Bangladesh</p>
+                                    <p>Age: 22 years</p>
+                                </div>
+                                <div className='mt-6 lg:mt-0'>
+                                    <p>E-Mail: kamruzzaman.md@hotmail.com</p>
+                                    <p>phone: +8801949016701</p>
+                                    <p>Linkedind: linkedin/.....</p>
+                                    <p>GitHub: ................</p>
+                                </div>
                             </div>
+                            <div className='flex justify-start my-4 lg:mt-0'>
+                                <button className='btn btn-outline btn-error font-bold hover:text-white mx-4'>live site</button>
+                                <button className='btn btn-outline btn-success font-bold text-white'>Code link</button>
+                            </div>
+
                         </div>
+                        
                     </div>
+
+
+
                     {/* modal closed button */}
-                    <div class="modal-action absolute right-0 bottom-0 p-4">
+                    <div class="modal-action fixed md:absolute z-10 right-0 bottom-0 p-4">
                         <label for="ModalDeatilsShowingTigger" class="btn">Close!</label>
                     </div>
                 </div>
