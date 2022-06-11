dfa34680d96c27066dc1d329e5769c51eb6b6f49
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-06-11 07:25:35 +0600

    skill section add

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './skill.css';
 import skillBGI from '../../assets/skills.svg';
+import skillImage from '../../assets/profile_photo.png';
 const Skills = () => {
     return (
         <div className='min-h-screen'>
@@ -10,34 +11,66 @@ const Skills = () => {
                 <hr className='my-4' />
             </div>
 
-            <div className='grid grid-cols-1 lg:grid-cols-2'>
-                <div class="skills bg-base-200">
-                    <div class="skill">
-                        <div class="skill-name">HTML</div>
-                        <div class="skill-bar">
-                            <div class="skill-per" per="90%" style={{ 'maxWidth': '90%' }}></div>
+            <div className="hero bg-cover bg-no-repeat bg-center">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className='w-full card'>
+                        <div class="skills bg-base-200">
+                            <div class="skill">
+                                <div class="skill-name">HTML</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="90%" style={{ 'maxWidth': '90%' }}></div>
+                                </div>
+                            </div>
+
+                            <div class="skill">
+                                <div class="skill-name">CSS</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="70%" style={{ 'maxWidth': '70%' }}></div>
+                                </div>
+                            </div>
+
+                            <div class="skill">
+                                <div class="skill-name">Bootstrap</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="90%" style={{ 'maxWidth': '90%' }}></div>
+                                </div>
+                            </div>
+                            <div class="skill">
+                                <div class="skill-name">tailwind</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="85%" style={{ 'maxWidth': '85%' }}></div>
+                                </div>
+                            </div>
+                            <div class="skill">
+                                <div class="skill-name">Javascript</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="65%" style={{ 'maxWidth': '65%' }}></div>
+                                </div>
+                            </div>
+                            <div class="skill">
+                                <div class="skill-name">React</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="75%" style={{ 'maxWidth': '75%' }}></div>
+                                </div>
+                            </div>
+                            <div class="skill">
+                                <div class="skill-name">node & express</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="40%" style={{ 'maxWidth': '45%' }}></div>
+                                </div>
+                            </div>
+                            <div class="skill">
+                                <div class="skill-name">MongoDB</div>
+                                <div class="skill-bar">
+                                    <div class="skill-per" per="40%" style={{ 'maxWidth': '40%' }}></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="skill">
-                        <div class="skill-name">CSS</div>
-                        <div class="skill-bar">
-                            <div class="skill-per" per="70%" style={{ 'maxWidth': '70%' }}></div>
-                        </div>
-                    </div>
-
-                    <div class="skill">
-                        <div class="skill-name">Javascript</div>
-                        <div class="skill-bar">
-                            <div class="skill-per" per="60%" style={{ 'maxWidth': '60%' }}></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='self-center	justify-self-center pt-20 lg:pt-0'>
-                    <img width={200} src={skillBGI} alt="" />
+                    <img src={`https://media.istockphoto.com/photos/hand-with-marker-writing-skill-concept-picture-id637711198?k=20&m=637711198&s=612x612&w=0&h=bznzNhH923ckyX6LINoDPtH6z3wbB0yO6Gvk5fQR0ms=`} className="w-full lg:w-1/2 rounded-lg shadow-3xl" alt='' />
                 </div>
             </div>
+
         </div>
     );
 };
